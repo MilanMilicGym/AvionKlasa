a82621a128e9efab419b398c6db29fb554304d63
MilanMilicGym <milanmilicgym@gmail.com>
2022-03-29 14:32:32 +0200

    Initial commit

---
+++ AvionKlasa/Avion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvionKlasa
+{
+    internal class Avion
+    {
+        public string ImeObjekta { get; set; }
+        public int BrojPutnika { get; set; }
+        public string NazivKompanije { get; set; }
+        public double SirinaKrila { get; set; }
+    }
+}
---
+++ AvionKlasa/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace AvionKlasa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var avion=new Avion();
+            avion.ImeObjekta = "Avio";
+
+            avion.BrojPutnika = 140;
+            avion.NazivKompanije = "Air Serbia";
+            avion.SirinaKrila = 15.32;
+            Console.WriteLine(avion.ImeObjekta + " ima " + avion.BrojPutnika + " broj putnika " + avion.SirinaKrila + " su mu sirina krila " + avion.NazivKompanije + " je naziv kompanije");
+
+            var helikopter = new Avion();
+             helikopter.ImeObjekta = "Helikopter";
+            Console.WriteLine(helikopter.ImeObjekta);
+        }
+    }
+}
