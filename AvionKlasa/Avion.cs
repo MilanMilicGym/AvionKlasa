e6256848695b9f3450f2fed4faa57e23b7df35df
MilanMilicGym <milanmilicgym@gmail.com>
2022-03-30 11:42:14 +0200

    Izmene klase

--- AvionKlasa/Avion.cs
+++ AvionKlasa/Avion.cs
@@ -12,5 +12,30 @@ namespace AvionKlasa
         public int BrojPutnika { get; set; }
         public string NazivKompanije { get; set; }
         public double SirinaKrila { get; set; }
+         public void Poletanje ()
+
+    {
+        
+
+           
+            {
+                Console.WriteLine(ImeObjekta + " kompanije " + NazivKompanije + " se sprema za poletanje");
+            }
     }
+        public void Letenje ()
+        {
+            Console.WriteLine(SirinaKrila + " je sirina krila " + " broj putnika je " + BrojPutnika + ImeObjekta + " trenutno leti");
+        }
+        public void Sletanje ()
+        {
+            Console.WriteLine(ImeObjekta + " se sprema za sletanje");
+        }
+        public void Parkiranje ()
+        {
+            Console.WriteLine(BrojPutnika + " putnika je bezbedno sletelo " + NazivKompanije + " Vam zeli ugodan dan");
+        }
+    }
+   
+
+
 }
--- AvionKlasa/Program.cs
+++ AvionKlasa/Program.cs
@@ -6,17 +6,20 @@ namespace AvionKlasa
     {
         static void Main(string[] args)
         {
-            var avion=new Avion();
-            avion.ImeObjekta = "Avio";
+            var avion = new Avion();
+            avion.ImeObjekta = "Avion";
 
             avion.BrojPutnika = 140;
             avion.NazivKompanije = "Air Serbia";
             avion.SirinaKrila = 15.32;
+
             Console.WriteLine(avion.ImeObjekta + " ima " + avion.BrojPutnika + " broj putnika " + avion.SirinaKrila + " su mu sirina krila " + avion.NazivKompanije + " je naziv kompanije");
 
-            var helikopter = new Avion();
-             helikopter.ImeObjekta = "Helikopter";
-            Console.WriteLine(helikopter.ImeObjekta);
+            avion.Poletanje();
+            avion.Letenje();
+            avion.Sletanje();   
+            avion.Parkiranje();
+
         }
     }
 }
